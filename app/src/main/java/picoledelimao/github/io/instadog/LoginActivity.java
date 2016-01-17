2dbe0337caa6fd056392500a7db488ab2448cea1
Abner Matheus <abner.math.c@gmail.com>
2016-01-17 03:16:55 -0200

    Added login and signup screens

---
+++ app/src/main/java/picoledelimao/github/io/instadog/LoginActivity.java
@@ -0,0 +1,34 @@
+package picoledelimao.github.io.instadog;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        Button btSignup = (Button) findViewById(R.id.btSignup);
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
