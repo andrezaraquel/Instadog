a881e9662a824338b96d4815df84569dfc781c73
unknown <andreza.queiroz@ccc.ufcg.edu.br>
2016-01-22 00:58:56 -0200

    Implementando cadastro e login

--- app/src/main/java/picoledelimao/github/io/instadog/LoginActivity.java
+++ app/src/main/java/picoledelimao/github/io/instadog/LoginActivity.java
@@ -1,6 +1,8 @@
 package picoledelimao.github.io.instadog;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,15 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 /**
  * This activity describes the actions taken on login screen
  * @author Abner M. C. Araujo
@@ -20,6 +31,10 @@ import android.widget.EditText;
  * @since 01.17.2016
  */
 public class LoginActivity extends AppCompatActivity {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient mClient;
+    private View mLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +71,66 @@ public class LoginActivity extends AppCompatActivity {
      * @param password
      *      Password filled in the password field
      */
-    private void login(String login, String password) {
-        // TODO Replace the body of this method with the correct implementation
+    private void login(final String login, final String password) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String url = "http://instadog-armq.rhcloud.com/login";
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("Login", login);
+                    json.put("senha", password);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                RequestBody body = RequestBody.create(JSON, json.toString());
+                Request request = new Request.Builder().url(url).post(body).build();
+                Response response = null;
+                try {
+                    response = mClient.newCall(request).execute();
+                    final String result = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (!result.equals("SUCESSO")) {
+                                new AlertDialog.Builder(LoginActivity.this)
+                                        .setTitle("Erro")
+                                        .setMessage(result)
+                                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                mLoading.setVisibility(View.GONE);
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+                            } else {
+                                new AlertDialog.Builder(LoginActivity.this)
+                                        .setTitle("Sucesso")
+                                        .setMessage("Login realizado com sucesso")
+                                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                                finish();
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
--- app/src/main/java/picoledelimao/github/io/instadog/SignupActivity.java
+++ app/src/main/java/picoledelimao/github/io/instadog/SignupActivity.java
@@ -1,5 +1,8 @@
 package picoledelimao.github.io.instadog;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * This activity describes the actions taken on signup screen
@@ -17,6 +32,11 @@ import android.widget.EditText;
  */
 public class SignupActivity extends AppCompatActivity {
 
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient mClient;
+    private View mLayout;
+    private View mLoading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +45,10 @@ public class SignupActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mClient = new OkHttpClient();
+
+        mLayout = findViewById(R.id.layout);
+        mLoading = findViewById(R.id.loading);
         Button btSignup = (Button) findViewById(R.id.btSignup);
         final EditText etLogin = (EditText) findViewById(R.id.etLogin);
         final EditText etEmail = (EditText) findViewById(R.id.etEmail);
@@ -33,11 +57,11 @@ public class SignupActivity extends AppCompatActivity {
         btSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            String login = etLogin.getText().toString();
-            String email = etEmail.getText().toString();
-            String password = etPassword.getText().toString();
-            String repeatPassword = etRepeatPassword.getText().toString();
-            signup(login, email, password, repeatPassword);
+                String login = etLogin.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String repeatPassword = etRepeatPassword.getText().toString();
+                signup(login, email, password, repeatPassword);
             }
         });
     }
@@ -53,8 +77,76 @@ public class SignupActivity extends AppCompatActivity {
      * @param repeatPassword
      *      Repeat password filled in the repeat password field
      */
-    private void signup(String login, String email, String password, String repeatPassword) {
-        // TODO Insert code with correct implementation here
+    private void signup(final String login, final String email, final String password, final String repeatPassword) {
+        {
+            if (!password.equals(repeatPassword)) {
+                new AlertDialog.Builder(SignupActivity.this)
+                        .setTitle("Erro")
+                        .setMessage("Senhas não coincidem")
+                        .setNeutralButton("OK", null)
+                        .create()
+                        .show();
+            } else {
+                mLoading.setVisibility(View.VISIBLE);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String url = "http://instadog-armq.rhcloud.com/cadastrar";
+                        JSONObject json = new JSONObject();
+                        try {
+                            json.put("login", login);
+                            json.put("email", email);
+                            json.put("senha", password);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        RequestBody body = RequestBody.create(JSON, json.toString());
+                        Request request = new Request.Builder().url(url).post(body).build();
+                        Response response = null;
+                        try {
+                            response = mClient.newCall(request).execute();
+                            final String result = response.body().string();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (!result.equals("SUCESSO")) {
+                                        new AlertDialog.Builder(SignupActivity.this)
+                                                .setTitle("Erro")
+                                                .setMessage(result)
+                                                .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        mLoading.setVisibility(View.GONE);
+                                                    }
+                                                })
+                                                .create()
+                                                .show();
+                                    } else {
+                                        new AlertDialog.Builder(SignupActivity.this)
+                                                .setTitle("Sucesso")
+                                                .setMessage("Cadastro realizado com sucesso")
+                                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                        finish();
+                                                    }
+                                                })
+                                                .create()
+                                                .show();
+                                    }
+                                }
+                            });
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
     }
 
 }
